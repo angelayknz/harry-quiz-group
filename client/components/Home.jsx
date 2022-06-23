f8e7820757da99b5fa1436652bb288475cefe714
ayoung-lee <103156367+ayoung-lee@users.noreply.github.com>
2022-06-24 11:32:25 +1200

    swap oveer the submit button and imgs on home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAllCharacters } from '../apiClient'
 
-export default function Home() {
+export default function Home(props) {
+  const [mainCharInfo, setMainCharInfo] = useState([])
+  const mainCharacters = [
+    'Hermione Jean Granger',
+    'Luna Lovegood',
+    'Minerva McGonagall',
+    'Bellatrix Lestrange',
+  ]
+
+  useEffect(() => {
+    getAllCharacters()
+      .then((characters) => {
+        const mains = []
+        characters.forEach((character) => {
+          if (mainCharacters.includes(character.character)) {
+            mains.push(character)
+          }
+        })
+        setMainCharInfo(mains)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }, [])
+
   return (
     <>
       <form>
         <h1>Harry Potter Quiz</h1>
         <label>
-          <input type="text" name="name" />
+          <input type="text" name="name" placeholder="Enter your name 🧙" />
         </label>
+        <h4>Choose your character picture from below</h4>
+
+        {mainCharInfo.map((char) => {
+          return (
+            <div key={char.id}>
+              <div>{char.character}</div>
+              <img src={char.image} alt={char.character} />
+            </div>
+          )
+        })}
+
         <input type="submit" value="Submit" />
       </form>
     </>
