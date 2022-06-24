a3b02d2c8d010eb80e4cdff15edfcc9f5b86fb2b
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-24 13:54:52 +1200

    random question console logging

--- client/arrayFunction.js
+++ client/arrayFunction.js
@@ -1,11 +1,14 @@
 const data = require('../server/db/questions.json')
-let questions = data.questions
-// console.log(questions)
 
+const questions = data.questions
 let randomQuestionIndex = Math.floor(Math.random() * 30)
-
 let randomQuestion = questions[randomQuestionIndex]
 let randomQuestionArray = []
-console.log(randomQuestionArray.push(randomQuestionIndex), randomQuestion)
-// console.log(dataValues[(1, 2)])
-// console.log(dataValues[Math.floor(Math.random() * dataValues.length)])
+
+const getRandomQuestion = () => {
+  if ([randomQuestionArray].includes(!randomQuestionIndex)) {
+    return randomQuestionArray.push(randomQuestionIndex), randomQuestion
+  } else randomQuestion
+}
+
+console.log(getRandomQuestion())
--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -4,8 +4,19 @@ import Question from './Question'
 
 export default function Questions(props) {
   const { questions, updateScore } = props
+
   // console.log(questions[0])
   const [questionNumber, setQuestionNumber] = useState(0)
+
+  let randomQuestionIndex = Math.floor(Math.random() * 30)
+  let randomQuestion = questions[randomQuestionIndex]
+  let randomQuestionArray = []
+
+  console.log(
+    randomQuestionArray.push(randomQuestionIndex),
+    randomQuestion,
+    randomQuestionIndex
+  )
 
   function changeQuestionNumber() {
     setQuestionNumber(questionNumber + 1)
